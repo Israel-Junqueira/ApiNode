32c155e23773deb51f70fbbc34099949c84ebbfe
Israel Ribeiro Junqueira <israelribeiro313@gmail.com>
2023-09-14 22:15:12 -0300

    MetodosCriados
    GetAll() e GettId

--- .ionic/api-app/src/app/tab1/tab1.page.ts
+++ .ionic/api-app/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, afterNextRender } from '@angular/core';
+import { Usuario } from '../models/Usuario.model';
+import { UsuarioService } from '../services/usuario.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  listaUsuarios : Usuario[] = [];
+  usuario?: Usuario;
+  id! : number;
 
+  //para consumir api devemos instanciar o serviço que foi criado
+  constructor(private userService:UsuarioService) {}
+
+  //Criar os metodos de trabalho
+  //Depois chamamos o metodo que desejamos usar
+  //utilizarmos o subscribe para aguardar o retorno da api
+  //primeiro chamamos o serviço atraves da sua varival
+  //criar uma varival para guardar o retorno
+  //enviamos essa variavel para uma função anonima.
+  buscarUsuario(){
+    this.userService.getAll().subscribe(retorno=>{
+      this.listaUsuarios = retorno as Usuario[];
+      console.log(this.listaUsuarios);
+    });
+
+  }
+
+  
+  buscarUsuarioID(){   
+    this.userService.getOne(this.id).subscribe(retorno =>{
+      console.log(retorno);
+      this.usuario = retorno as Usuario;
+      this.listaUsuarios = [];
+    });
+  }
 }
